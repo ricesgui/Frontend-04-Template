76d80954caba1ab23e8c161a9c3365278e1b6bd6
hwjavatoxg@163.com <hwjavatoxg@163.com>
2020-12-27 20:06:10 +0800

    week17

---
+++ Week_17/generators/app/template/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="app">{{ msg }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      msg: "Hi",
+    };
+  },
+};
+</script>
+<style scoped>
+.app {
+  font-size: 20px;
+  text-align: center;
+}
+</style>
---
+++ Week_17/generators/app/template/test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('test cases:', function () {
+  it('3+2=5', function () {
+    assert.equal(3 + 2, 5)
+  })
+})
